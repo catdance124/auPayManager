416dba5a1ae94d20a7880eacfadb63bcd1a17d1a
catdance124 <kinosi.cat@gmail.com>
2024-02-08 18:29:59 +0900

    npm scriptでtest,build,deploy

--- deployment/generate_claspjson.sh
+++ deployment/generate_claspjson.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-CLASPJSON=$(cat <<-END
-    {
-        "scriptId": "$SCRIPT_ID",
-        "rootDir": "public"
-    }
-END
-)
+CONFIG_FILE=./.clasp.json
 
-echo $CLASPJSON > ~/.clasp.json
+# ファイルが存在しない場合のみ作成する
+if [ ! -f "$CONFIG_FILE" ]; then
+    source .env
+    CLASPJSON=$(cat <<-END
+        {
+            "scriptId": "$SCRIPT_ID",
+            "rootDir": "public"
+        }
+END
+    )
+
+    echo $CLASPJSON > "$CONFIG_FILE"
+fi
--- deployment/generate_clasprc.sh
+++ deployment/generate_clasprc.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
 
-LOGIN=$(cat <<-END
-    {
-        "token": {
-            "access_token": "$ACCESS_TOKEN",
-            "refresh_token": "$REFRESH_TOKEN",
-            "scope": "https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/service.management https://www.googleapis.com/auth/script.deployments https://www.googleapis.com/auth/logging.read https://www.googleapis.com/auth/script.webapp.deploy https://www.googleapis.com/auth/userinfo.profile openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/script.projects https://www.googleapis.com/auth/drive.metadata.readonly",
-            "token_type": "Bearer",
-            "id_token": "$ID_TOKEN",
-            "expiry_date": 1686667610873
-        },
-        "oauth2ClientSettings": {
-            "clientId": "$CLIENT_ID",
-            "clientSecret": "$CLIENT_SECRET",
-            "redirectUri": "http://localhost"
-        },
-        "isLocalCreds": false
-    }
-END
-)
+CONFIG_FILE=~/.clasprc.json
+source .env
 
-echo $LOGIN > ~/.clasprc.json
+# ファイルが存在しない場合のみ作成する
+if [ ! -f "$CONFIG_FILE" ]; then
+    LOGIN=$(cat <<-END
+        {
+            "token": {
+                "access_token": "$ACCESS_TOKEN",
+                "refresh_token": "$REFRESH_TOKEN",
+                "scope": "https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/service.management https://www.googleapis.com/auth/script.deployments https://www.googleapis.com/auth/logging.read https://www.googleapis.com/auth/script.webapp.deploy https://www.googleapis.com/auth/userinfo.profile openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/script.projects https://www.googleapis.com/auth/drive.metadata.readonly",
+                "token_type": "Bearer",
+                "id_token": "$ID_TOKEN",
+                "expiry_date": 1686667610873
+            },
+            "oauth2ClientSettings": {
+                "clientId": "$CLIENT_ID",
+                "clientSecret": "$CLIENT_SECRET",
+                "redirectUri": "http://localhost"
+            },
+            "isLocalCreds": false
+        }
+END
+    )
+
+    echo $LOGIN > "$CONFIG_FILE"
+fi
